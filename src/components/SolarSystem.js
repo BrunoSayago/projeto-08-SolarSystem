421be76768863b351db695943dc9d5263822a6f6
Bruno Sayago <brunossgripp@gmail.com>
2022-08-18 15:45:01 -0300

    Requisitos 5 e 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planet) => {
+          const planetN = planet.name;
+          const planetI = planet.image;
+          return (
+            <PlanetCard key={ planetN } planetName={ planetN } planetImage={ planetI } />
+          );
+        })}
       </div>
     );
   }
