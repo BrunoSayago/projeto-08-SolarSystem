d80ca3a8597a8087f146e2e2bde9f905ba5c88ea
Bruno Sayago <brunossgripp@gmail.com>
2022-08-18 16:02:27 -0300

    Requisitos 9 e 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => {
+          const { name, year, country, destination } = mission;
+          return (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          );
+        })}
       </div>
     );
   }
